102da999b63b66ab7cb913f20e56da7eece7b92a
thatboyreegan <reaganronald491@gmail.com>
2025-02-02 17:29:28 +0300

    add: Booking detail page implementation

---
+++ interfaces/index.ts
@@ -0,0 +1,28 @@
+export interface CardProps {}
+
+export interface ButtonProps {}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+
+export interface PropertyCategoriesProps {
+    id: number;
+    imageUrl: string;
+    title: string;
+}
